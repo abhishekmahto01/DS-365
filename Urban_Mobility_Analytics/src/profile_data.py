d12de4fd03fba853992727cd52b26040c2c795c5
abhishekmahto01 <gumnamusafir584@gmail.com>
2026-08-06 23:48:02 +0530

    Day 4: Add reusable data loading and profiling functions

--- Urban_Mobility_Analytics/src/profile_data.py
+++ Urban_Mobility_Analytics/src/profile_data.py
@@ -1,48 +1,66 @@
 from sqlalchemy import create_engine
 import pandas as pd
 
-
+# Database Configuration
 USERNAME = "abhishekmahto"
 PASSWORD = "PASSWORD"
-HOST="localhost"
-PORT="5432"
-DATABASE="urban_mobility_analytics"
+HOST = "localhost"
+PORT = "5432"
+DATABASE = "urban_mobility_analytics"
 
+# Create Engine
 engine = create_engine(
     f"postgresql+psycopg2://{USERNAME}:{PASSWORD}@{HOST}:{PORT}/{DATABASE}"
 )
 
-folder = "/Users/abhishekmahto/Desktop/archive (4)"
+# -----------------------------
+# Functions
+# -----------------------------
+
+def load_data(engine, table_name):
+    query = f"SELECT * FROM {table_name};"
+    return pd.read_sql(query, engine)
 
 
-with engine.connect() as connection:
+def profile_dataset(df):
+    print("\nShape:")
+    print(df.shape)
+
+    print("\nInfo:")
+    df.info()
+
+    print("\nSummary Statistics:")
+    print(df.describe(include="all"))
+
+    print("\nMissing Values:")
+    print(df.isnull().sum())
+
+
+# -----------------------------
+# Main Program
+# -----------------------------
+
+with engine.connect():
     print("Connected to the database successfully!")
 
-
 query = """
-select table_name
-from information_schema.tables
-where table_schema ='public';
+SELECT table_name
+FROM information_schema.tables
+WHERE table_schema = 'public';
 """
 
-tables_df=pd.read_sql(query,engine)
-print("Tables in Database:")
+tables_df = pd.read_sql(query, engine)
 
+print("\nTables in Database:")
+print(tables_df)
+
+# First table name
 first_table = tables_df["table_name"].iloc[0]
 
-print(first_table)
+print(f"\nLoading Table: {first_table}")
 
+# Load Data
+df = load_data(engine, first_table)
 
-
-
-
-query = """
-SELECT *
-FROM other_american_b01362;
-"""
-
-df = pd.read_sql(query, engine)
-print(df.shape)
-print(df.columns)
-print(df.dtypes)
-print(df.head())
+# Profile Dataset
+profile_dataset(df)
